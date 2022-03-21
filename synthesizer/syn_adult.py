fc85a968d980913431d80bc93c62f41812d48472
mshubhankar <s3mohapatra@uwaterloo.ca>
2022-03-21 12:16:01 -0400

    add adult multiple missing mechs

--- synthesizer/syn_adult.py
+++ synthesizer/syn_adult.py
@@ -17,11 +17,11 @@ def syn_adult():
     start = time.time()
 
     orig_data = f'./testdata/adult/adult.csv'
-    path_data = f'./testdata/adult/adult_missing_0.1.csv'
+    path_data = f'./testdata/adult/MAR/adult_missing_0.1.csv'
     path_ic = f'./testdata/adult/adult.ic'
 
     path_data_preproc = preproc_adult(path_data)
-    n_row, n_col = pd.read_csv(path_data_preproc).shape
+    n_row, n_col = pd.read_csv(path_data_preproc).dropna().shape
     n_len = len(str(n_row)) + 1
 
     # ## eps=0.1
@@ -90,18 +90,19 @@ def syn_adult():
 
     ### eps=1
     paras = {
-        'reuse_embedding': True,  # set True to reuse the embedding
+        'reuse_embedding': False,  # set True to reuse the embedding
         'dp': True,  # set True to enable privacy
         'n_row': n_row,  # number of rows in the true dataset
         'n_col': n_col,  # number of columns in the true dataset
         'epsilon1': .1,  #
         'l2_norm_clip': 1.0,
-        'noise_multiplier': 1.1,  #
+        'noise_multiplier': 1.1,  #1.1-no missing 2.6-10% 15-20%
         'minibatch_size': 23,  # batch size to sample for each iteration
         'microbatch_size': 1,  # micro batch size
         'delta': float(f'1e-{n_len}'),  # depends on data size. Do not change for now
         'learning_rate': 1e-4,
-        'impute' : True,
+        'impute' : False,
+        'complete_intermediate' : True,
         'iterations': 1600  # =1600 for eps=1
         # 'iterations': 1  # testing
     }
